9c5a31f3056ddc940bd4b173faab13d10dd0b4ba
Shota Itakura <1531860+itkr@users.noreply.github.com>
2025-05-06 22:41:33 +0900

    ScrollArea追加

--- web/src/app/top/page.tsx
+++ web/src/app/top/page.tsx
@@ -11,14 +11,16 @@ import {
 import { ChatMessageList } from "@/components/ui/chat/chat-message-list";
 import { ChatInput } from "@/components/ui/chat/chat-input";
 import { Button } from "@/components/ui/button";
-import {
-  Paperclip,
-  Microphone,
-  CornerDownLeft,
-  Like,
-  Dislike,
-  Question,
-} from "@mynaui/icons-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+//import {
+//  Paperclip,
+//  Microphone,
+//  CornerDownLeft,
+//  Like,
+//  Dislike,
+//  Question,
+//} from "@mynaui/icons-react";
 
 const metadata = {
   title: "Top",
@@ -32,26 +34,6 @@ type Message = {
   isLoading?: boolean;
 };
 
-// const messages = [
-//   {
-//     id: 1,
-//     message: "Hello, how has your day been? I hope you are doing well.",
-//     sender: "user",
-//   },
-//   {
-//     id: 2,
-//     message:
-//       "Hi, I am doing well, thank you for asking. How can I help you today?",
-//     sender: "bot",
-//   },
-//   {
-//     id: 3,
-//     message: "",
-//     sender: "bot",
-//     isLoading: true,
-//   },
-// ];
-
 const firstBotMessage: Message = {
   id: 1,
   message: "Hello, how has your day been? I hope you are doing well.",
@@ -60,20 +42,24 @@ const firstBotMessage: Message = {
 
 const actionIcons = [
   {
-    icon: () => (<Like className="size-4" />),
+    // icon: () => (<Like className="size-4" />),
+    icon: () => <span className="size-4">👍</span>,
     type: "like",
   },
   {
-    icon: () => (<Dislike className="size-4" />),
+    // icon: () => (<Dislike className="size-4" />),
+    icon: () => <span className="size-4">👎</span>,
     type: "dislike",
   },
   {
-    icon: () => (<Question className="size-4" />),
+    // icon: () => (<Question className="size-4" />),
+    icon: () => <span className="size-4">❓</span>,
     type: "question",
   },
 ];
 
 const Page: FC = () => {
+  const [composing, setIsComposing] = useState(false);
   const [messages, setMessages] = useState<Message[]>([firstBotMessage]);
   const [loadingBotMessage, setLoadingBotMessage] = useState(false);
 
@@ -96,7 +82,7 @@ const Page: FC = () => {
       setMessages((prev) => [...prev, botMessage]);
       setLoadingBotMessage(false);
     }, 2000);
-  }
+  };
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -106,69 +92,93 @@ const Page: FC = () => {
     if (message.trim() === "") return;
     onSendMessage(message);
     form.reset();
-  }
+  };
 
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">Top Page</h1>
       <p>This is the top page.</p>
       <div>
-        <ChatMessageList>
-          {messages.map((message, index) => {
-            const variant = message.sender === "user" ? "sent" : "received";
-            return (
-              <ChatBubble key={message.id} variant={variant}>
-                <ChatBubbleAvatar fallback={variant === "sent" ? "US" : "AI"} />
-                <ChatBubbleMessage isLoading={message.isLoading}>
-                  {message.message}
+        <ScrollArea className="h-[400px] w-full rounded-lg border bg-background">
+          <ChatMessageList>
+            {messages.map((message, index) => {
+              const variant = message.sender === "user" ? "sent" : "received";
+              return (
+                <ChatBubble key={message.id} variant={variant}>
+                  <ChatBubbleAvatar
+                    fallback={variant === "sent" ? "US" : "AI"}
+                  />
+                  <ChatBubbleMessage isLoading={message.isLoading}>
+                    {message.message}
+                  </ChatBubbleMessage>
+                  {/* Action Icons */}
+                  <ChatBubbleActionWrapper>
+                    {actionIcons.map(({ icon: Icon, type }) => (
+                      <ChatBubbleAction
+                        className="size-7"
+                        key={type}
+                        icon={<Icon />}
+                        onClick={() =>
+                          console.log(
+                            "Action " + type + " clicked for message " + index,
+                          )
+                        }
+                      />
+                    ))}
+                  </ChatBubbleActionWrapper>
+                </ChatBubble>
+              );
+            })}
+            {loadingBotMessage && (
+              <ChatBubble variant="received">
+                <ChatBubbleAvatar fallback="AI" />
+                <ChatBubbleMessage isLoading={true}>
+                  This is a loading message from the bot.
                 </ChatBubbleMessage>
-                {/* Action Icons */}
-                <ChatBubbleActionWrapper>
-                  {actionIcons.map(({ icon: Icon, type }) => (
-                    <ChatBubbleAction
-                      className="size-7"
-                      key={type}
-                      icon={<Icon />}
-                      onClick={() =>
-                        console.log(
-                          "Action " + type + " clicked for message " + index,
-                        )
-                      }
-                    />
-                  ))}
-                </ChatBubbleActionWrapper>
               </ChatBubble>
-            );
-          })}
-          {loadingBotMessage && (
-            <ChatBubble variant="received">
-              <ChatBubbleAvatar fallback="AI" />
-              <ChatBubbleMessage isLoading={true}>
-                This is a loading message from the bot.
-              </ChatBubbleMessage>
-            </ChatBubble>
-          )}
-        </ChatMessageList>
+            )}
+          </ChatMessageList>
+        </ScrollArea>
       </div>
       <div>
-        <form className="relative rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring p-1" onSubmit={onSubmit}>
+        <form
+          className="relative rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring p-1"
+          onSubmit={onSubmit}
+        >
           <ChatInput
             placeholder="Type your message here..."
             className="min-h-12 resize-none rounded-lg bg-background border-0 p-3 shadow-none focus-visible:ring-0"
             name="message"
+            onKeyDown={(e) => {
+              if (e.key !== "Enter") return;
+              if (e.shiftKey) return;
+              if (composing) return;
+              e.preventDefault();
+              const form = e.currentTarget.form as HTMLFormElement;
+              const formData = new FormData(form);
+              const message = formData.get("message") as string;
+              if (message.trim() === "") return;
+              onSendMessage(message);
+              form.reset();
+            }}
+            onCompositionStart={() => setIsComposing(true)}
+            onCompositionEnd={() => setIsComposing(false)}
           />
           <div className="flex items-center p-3 pt-0">
             <Button variant="ghost" size="icon">
-              <Paperclip className="size-4" />
+              {/* <Paperclip className="size-4" /> */}
+              <span className="size-4">📎</span>
               <span className="sr-only">Attach file</span>
             </Button>
             <Button variant="ghost" size="icon">
-              <Microphone className="size-4" />
+              {/* <Microphone className="size-4" /> */}
+              <span className="size-4">🎤</span>
               <span className="sr-only">Use Microphone</span>
             </Button>
             <Button size="sm" className="ml-auto gap-1.5" type="submit">
               Send Message
-              <CornerDownLeft className="size-3.5" />
+              {/* <CornerDownLeft className="size-3.5" /> */}
+              <span className="size-3.5">➡️</span>
             </Button>
           </div>
         </form>
