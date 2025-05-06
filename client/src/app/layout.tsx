7e2e9a8ff891443671ff96faf83b5dddc3fcd2b4
Shota Itakura <1531860+itkr@users.noreply.github.com>
2025-05-06 16:09:22 +0900

    Chakra UIに変更

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Provider } from "@/components/ui/provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -23,11 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
