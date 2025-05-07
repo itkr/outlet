ba88fbbd3da877f5530a762eac3f35057c941cf6
Shota Itakura <1531860+itkr@users.noreply.github.com>
2025-05-07 23:59:27 +0900

    Add layout

--- web/src/app/top/layout.tsx
+++ web/src/app/top/layout.tsx
@@ -1,12 +1,21 @@
 import { Metadata } from "next";
-import { Inter } from "next/font/google";
-// import './globals.css';
+// import { Inter } from "next/font/google";
 import { FC, ReactNode } from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 // const inter = Inter({ subsets: ['latin'] });
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className="container mx-auto p-4">{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      {/* <main> */}
+      <SidebarTrigger />
+      <div className="container mx-auto p-4">{children}</div>
+      {/* </main> */}
+    </SidebarProvider>
+  );
 };
 
 export const metadata: Metadata = {
